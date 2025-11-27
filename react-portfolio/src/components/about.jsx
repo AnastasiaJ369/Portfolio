d8d1b038fa033f5c3809237d39a20381f61bb5b4
Anastasia <anastasia.m.johnson20@gmail.com>
2025-11-27 11:35:43 -0500

    About Me section

--- react-portfolio/src/components/about.jsx
+++ react-portfolio/src/components/about.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Navbar from './navbar.jsx'
 
+
 const About = () => {
   return (
      <section id="About" className='pt-20'>
       <Navbar />
         <div className='text-4xl font-bold mx-8 mb-4 shadow-xl shadow-cyan-500/50'>
-            <div className='text-white'>
+            <div className='about-title text-white mt-8'>
             About Me
             </div>
-            <div className='text-lg mt-4 text-white'>
-              Hello! I'm Anastasia, a passionate web developer with a love for creating beautiful 
-              and functional websites. With a strong foundation in HTML, CSS, and JavaScript, I 
-              enjoy bringing ideas to life in the digital world. When I'm not coding, you can find
-               me exploring new technologies, reading tech blogs, or hiking in nature.
+            <div className='about-subtext text-white mt-8 text-md text-3xl'>
+              Hello! I'm Anastasia, a budding software developer from Daytona Beach, Florida. I'm 
+              currently pursuing an A.S. degree in Computer Analysis and Programming with the
+              expectation of pursuing a B.S. in Information Technology. With a foundation in HTML, CSS,
+              Javascript and now React, I'm eager to continue to expand my skills and take on any new 
+              challenges that may come my way. When I'm not coding, you can find me reading, kayaking
+              and hiking, or dedicating time to my family and friends.
             </div>
         </div>
      </section>
